a7dc72d9c956c00de3c25266ffe1f2fac5f11763
JosiahAppert <appertj@oregonstate.edu>
2025-11-26 10:38:29 -0800

    finally found fix for icon

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  icons: [{src: './public/baseball.svg',
+           sizes: '32x32',
+           type: 'image/svg+xml'
+  }]
 })
